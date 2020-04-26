bb08e40e9e61508a2a8c71070d22cf52c1f4dcf7
rishabh-nandwana <36503538+rishabh-nandwana@users.noreply.github.com>
2020-04-26 14:58:14 +0530

    Add files via upload

---
+++ w3_a1_ Matrix_Generator.py
@@ -0,0 +1,13 @@
+a = input("Enter l: ")
+b = input("Enter m: ")
+c = input("Enter n: ")
+mat  = [[["*" for i in range(c)] for  j in range(b)] for k in range(a)]
+print(mat)
+# result=[]
+# # for i in range(2):
+# # 	result.append(m)
+# # print(result)
+# for x in range(len(m)):
+# 	print "\n"
+# 	result.append(m)
+# print result
